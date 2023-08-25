2b0b163a3c1fc916a2cdd006cb97c9476054168e
Freja <frejaeb@gmail.com>
2023-08-25 12:45:34 +0200

    first commit

---
+++ freja.js
@@ -0,0 +1,16 @@
+{
+  powers: ["ild", "våben"];
+  realName: "Maria";
+  alias: "Bibli";
+  origin: "Holland";
+  weaknesses: ["bobler", "bøger"];
+  active: true;
+  isEvil: true;
+  height: 168;
+  birtday: {
+    day: 14;
+    month: 2;
+    year: 1990;
+  }
+  image: "https://images.pexels.com/photos/162520/farmer-man-shepherd-dog-162520.jpeg?auto=compress&cs=tinysrgb&w=600";
+}
